cf8a17b8c69e8892f95e33342da52fb82b27e088
Lydia Kimata <lydiakimata2@gmail.com>
2022-06-25 16:20:25 +0300

    Create 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" This module defines a class 'Square' """
+
+
+class Square:
+    """ Definition of a square by its size. """
+
+    def __init__(self, size=0):
+        """ Initialize size attribute with validation. """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Return the area of the square. """
+        return self.__size ** 2
