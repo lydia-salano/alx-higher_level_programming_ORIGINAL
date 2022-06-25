1c8b2ae340132c21f68d146385677b1db3b033de
Lydia Kimata <lydiakimata2@gmail.com>
2022-06-25 16:12:18 +0300

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" This module defines a class 'Square' """
+
+
+class Square:
+    """ Definition of a square by its size. """
+
+    def __init__(self, size):
+        """ Initialize size attribute. """
+        self.__size = size
