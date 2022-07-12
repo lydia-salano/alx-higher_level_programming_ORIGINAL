6240e60bd8d2ba35b4bada6930f411b092e0d2c7
lydia-salano <lydiakimata2@gmail.com>
2022-07-12 12:07:07 +0300

    advanced

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" This module defines a locked class. """
+
+
+class LockedClass:
+    """
+    Prevent the user from dynamically creating new
+    instance attributes other than first_name.
+    """
+    __slots__ = ['first_name']
